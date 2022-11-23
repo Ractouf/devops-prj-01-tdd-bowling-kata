6343dbd34dc97892d51f8dc359a5f436abff39fb
francois.vandeputte <francois.vandeputte@student.vinci.be>
2022-11-23 17:42:18 +0100

    refactor: readability

--- src/bowling-game.js
+++ src/bowling-game.js
@@ -12,20 +12,39 @@ class BowlingGame {
     let rollIndex = 0;
 
     for (let i = 0; i < 10; i++) {
-      if (this.rolls[rollIndex] === 10) {
-        score += 10 + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
+      if (this.isStrike(rollIndex)) {
+        score += this.strikeBonus(rollIndex);
         rollIndex++;
         continue;
       }
 
-      let frameScore = this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+      let frameScore = this.sumFrame(rollIndex);
 
-      if (frameScore === 10) score += frameScore + this.rolls[rollIndex + 2];
+      if (this.isSpare(frameScore)) score += this.spareBonus(rollIndex);
       else score += frameScore;
 
       rollIndex += 2;
     }
     return score;
+  }
+
+  sumFrame(rollIndex) {
+    return this.rolls[rollIndex] + this.rolls[rollIndex + 1];
+  }
+
+  spareBonus(rollIndex) {
+    return 10 + this.rolls[rollIndex + 2];
+  }
+  strikeBonus(rollIndex) {
+    return 10 + this.rolls[rollIndex + 1] + this.rolls[rollIndex + 2];
+  }
+
+  isSpare(frameScore) {
+    return frameScore === 10;
+  }
+
+  isStrike(rollIndex) {
+    return this.rolls[rollIndex] === 10;
   }
 }
 
