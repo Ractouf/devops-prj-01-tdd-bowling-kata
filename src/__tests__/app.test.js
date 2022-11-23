84d9825be53fe862c55e78b2d0d277dc1bcfd6de
francois.vandeputte <francois.vandeputte@student.vinci.be>
2022-11-23 16:31:20 +0100

    feat: init bowling game

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -1,8 +1,1 @@
-const { getCurrentMonth } = require('../app');
 
-describe('app tests suites - getCurrentMonth', () => {
-    test('should return the current month', () => {
-        const result = getCurrentMonth();
-        expect(result).toBe('November');
-    });
-});
---
+++ src/bowling-game.js
@@ -0,0 +1,5 @@
+let game;
+
+beforeEach(() => {
+    game = new BowlingGame();
+});
