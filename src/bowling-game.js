c0613485fa5fbfd5f82f66b5786d8f916bed7b70
francois.vandeputte <francois.vandeputte@student.vinci.be>
2022-11-23 16:48:48 +0100

    feat: game of 0's

--- src/bowling-game.js
+++ src/bowling-game.js
@@ -1,5 +1,25 @@
-let game;
+class BowlingGame {
 
-beforeEach(() => {
-    game = new BowlingGame();
-});
+    constructor() {
+        this.rolls = [];
+    }
+
+    roll(pins) {
+        this.rolls.push(pins);
+    }
+
+    get score() {
+        let score = 0;
+        let rollIndex = 0;
+
+        for (let i = 0; i < 10; i++) {
+            let frameScore = this.rolls[rollIndex] + this.rolls[rollIndex + 1]
+
+            score += frameScore;
+            rollIndex += 2;
+        }
+        return score;
+    }
+}
+
+module.exports = BowlingGame
